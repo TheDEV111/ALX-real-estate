0e41fd697d99f463ebe81c1bb0a20c2c8ff0e38f
Folashade Bello <folashadebello.b@gmail.com>
2026-04-16 14:37:04 +0100

    Navbar and Topbar

---
+++ Frontend/src/pages/Homepage.tsx
@@ -0,0 +1,14 @@
+import Topbar from "../components/Topbar"
+import Navbar from "../components/Navbar"
+
+function Homepage() {
+  return (
+    <div>
+      <Topbar />
+      <Navbar />
+
+    </div>
+  )
+}
+
+export default Homepage
