375c166370043192218e4679a1f183f2a866d248
Folashade Bello <folashadebello.b@gmail.com>
2026-05-29 12:13:25 +0100

    feat: Add responsive footer component with links and policies

--- Frontend/src/pages/Homepage.tsx
+++ Frontend/src/pages/Homepage.tsx
@@ -4,6 +4,7 @@ import PropertyTypeFilter from "../components/PropertyTypeFilter";
 import Hero from "../components/Hero";
 import PropertyListing from "../components/PropertyListingFilter";
 import { PropertyListings } from "../components/PropertyListings";
+import Footer from "../components/Footer";
 
 function Homepage() {
   return (
@@ -14,6 +15,8 @@ function Homepage() {
       <Hero />
       <PropertyListing />
       <PropertyListings />
+      <div className="bg-[#34967C] h-6"></div>
+      <Footer />
     </div>
   );
 }
